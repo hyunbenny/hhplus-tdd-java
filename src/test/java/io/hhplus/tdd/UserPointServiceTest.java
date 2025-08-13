d348c43a299200d9c262e25b1f1547f5009c8a02
hyunbenny <bluesky3268@naver.com>
2025-08-13 22:59:25 +0900

    test: UserPointService 테스트 클래스 추가
    UserPoint 관련 기능 구현을 위한 테스트 클래스를 추가함

---
+++ src/test/java/io/hhplus/tdd/UserPointServiceTest.java
@@ -0,0 +1,16 @@
+package io.hhplus.tdd;
+
+
+import io.hhplus.tdd.database.UserPointTable;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UserPointServiceTest {
+
+    @Mock
+    UserPointTable userPointTable;
+
+
+}
