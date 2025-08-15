763ddea28f31340222c653504ffc99cc2df821c1
hyunbenny <bluesky3268@naver.com>
2025-08-15 19:36:15 +0900

    feat: 포인트 조회, 사용, 포인트 이력 조회 기능 구현
    - 포인트 사용은 포인트 충전과 동일하게 포인트 사용 > 사용 이력을 쌓도록 구현함.
      - 포인트 사용 > 사용 이력 로직 중간 예외 발생 시, 기존의 포인트 데이터로 다시 업데이트 하도록 구현

--- src/main/java/io/hhplus/tdd/point/UserPointFacade.java
+++ src/main/java/io/hhplus/tdd/point/UserPointFacade.java
@@ -4,12 +4,18 @@ import io.hhplus.tdd.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserPointFacade {
 
     private final UserPointService userPointService;
     private final PointHistoryService pointHistoryService;
+
+    public UserPoint getPoint(long id) {
+        return userPointService.getPoint(id);
+    }
 
     public UserPoint chargePoint(long id, long chargePoint) {
         UserPoint originUserPoint = userPointService.getPoint(id);
@@ -24,6 +30,18 @@ public class UserPointFacade {
     }
 
     public UserPoint usePoint(long id, int usePoint) {
-        return null;
+        UserPoint originUserPoint = userPointService.getPoint(id);
+        try {
+            UserPoint chargedUserPoint = userPointService.usePoint(id, usePoint);
+            pointHistoryService.savePointHistory(id, usePoint, TransactionType.USE);
+            return chargedUserPoint;
+        } catch (CustomException e) {
+            userPointService.rollback(id, originUserPoint.point());
+            throw e;
+        }
+    }
+
+    public List<PointHistory> getUserPointHistories(long userId) {
+        return pointHistoryService.getUserPointHistories(userId);
     }
 }
