7e1df92f5d53648269f39b946c506875a1788159
hyunbenny <bluesky3268@naver.com>
2025-08-15 16:31:21 +0900

    feat: 포인트 이력 생성 기능 구현

--- src/main/java/io/hhplus/tdd/point/PointHistoryService.java
+++ src/main/java/io/hhplus/tdd/point/PointHistoryService.java
@@ -1,9 +1,13 @@
 package io.hhplus.tdd.point;
 
 import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.exception.CustomException;
+import io.hhplus.tdd.exception.InvalidTransactionTypeException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.security.InvalidParameterException;
 import java.util.List;
 
 @Service
@@ -17,7 +21,8 @@ public class PointHistoryService {
     }
 
 
-    public PointHistory savePointHistory(long userId, long chargePoint, TransactionType type) {
-        return null;
+    public PointHistory savePointHistory(long userId, long point, TransactionType type) {
+        if(type == null) throw new InvalidTransactionTypeException();
+        return pointHistoryTable.insert(userId, point, type, System.currentTimeMillis());
     }
 }
