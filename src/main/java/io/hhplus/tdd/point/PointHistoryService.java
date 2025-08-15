779658d93cbb878211cc717bbd4f72bcef91a6ca
hyunbenny <bluesky3268@naver.com>
2025-08-15 18:57:13 +0900

    refactor: UserPointFacade 사용을 위해 기존 서비스 접근제어자 수정
    - 외부에서는 UserPointFacade를 통해 접근하여 데이터를 처리할 수 있도록 하기 위해서 PointHistoryService와 UserPointService의 접근제어자를 public에서 default(private-package)로 변경함.

--- src/main/java/io/hhplus/tdd/point/PointHistoryService.java
+++ src/main/java/io/hhplus/tdd/point/PointHistoryService.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class PointHistoryService {
+class PointHistoryService {
 
     private final PointHistoryTable pointHistoryTable;
 
--- src/main/java/io/hhplus/tdd/point/UserPointService.java
+++ src/main/java/io/hhplus/tdd/point/UserPointService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserPointService {
+class UserPointService {
 
     private final UserPointTable userPointTable;
 
